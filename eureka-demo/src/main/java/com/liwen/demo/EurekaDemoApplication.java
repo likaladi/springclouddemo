61773b14ca665b0dea7cc8a10dabb745dadcfac1
likaladi <1401112764@qq.com>
2019-01-29 10:16:41 +0800

    Update EurekaDemoApplication.java

--- eureka-demo/src/main/java/com/liwen/demo/EurekaDemoApplication.java
+++ eureka-demo/src/main/java/com/liwen/demo/EurekaDemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication  //哈哈哈
 @EnableEurekaServer // 声明这个应用是一个EurekaServer
 public class EurekaDemoApplication {
 
